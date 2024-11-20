55ededf144cfe6410de00443134b4e72e45a9c2b
202010850 김준형 <jh010303@naver.com>
2024-11-20 16:53:22 +0900

    back 11/20
    back

--- sensor/urls.py
+++ sensor/urls.py
@@ -4,17 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('getTemp/<int:cnt>', views.getTemp, name='getTemp'),
-    path('getHumi/<int:cnt>', views.getHumi, name='getHumi'),
-    path('getVib/<int:cnt>', views.getVib, name='getVib'),
-    path('getProx/<int:cnt>', views.getProx, name='getProx'),
-    path('getLightD/<int:cnt>', views.getLightD, name= 'getLightD'),
-    path('getirDetect/<int:cnt>', views.getirDetect, name= 'getirDetect'),
-
-    path('setTemp', views.setTemp, name='setTemp'),
-    path('setHumi', views.setHumi, name='setHumi'),
-    path('setVib', views.setVib, name='setVib'),
-    path('setProx', views.setProx, name='setProx'),
-    path('setLightD', views.setLightD, name= 'setLightD'),
-    path('setirDetect', views.setirDetect, name= 'setirDetect'),
+    path('getSensor/<int:cnt>', views.getSensor, name='getsensor'),
+    path('setSensor', views.setSensor, name='setsensor'),
 ]
--- sensor/views.py
+++ sensor/views.py
@@ -1,87 +1,28 @@
-from turtle import right
 from django.http import HttpResponse
-from .models import TemperatureSensor as Temp, HumiditySensor as Humi, VibratorSensor as Vib, ProximitySensor as Prox, LightSensor as LightD, DetectorSensor as Detector
+from .models import DetectSensor as Sens
 from django.shortcuts import render
 from django.core import serializers
 from django.http import JsonResponse
+from django.utils import timezone
 import random
 import json
 
-def index(request):
-    sensor_value_list = Temp.objects.all().order_by('-reg_date').values()[:5]
+def index(request): 
+    sensor_value_list = Sens.objects.all().order_by('-reg_date').values()[:5]
     context = {
         'sensor_value_list': sensor_value_list,
     }
     return render(request, 'sensor/index.html', context)
 
-def getTemp(request, cnt):
-    results = list(Temp.objects.all().order_by('-reg_date').values())[:cnt][::-1]
-    return JsonResponse(results, safe=False)
-
-def getHumi(request, cnt):
-    results = list(Humi.objects.all().order_by('-reg_date').values())[:cnt][::-1]
-    return JsonResponse(results, safe=False)
-
-def getVib(request, cnt):
-    results = list(Vib.objects.all().order_by('-reg_date').values())[:cnt][::-1]
-    return JsonResponse(results, safe=False)
-
-def getProx(request, cnt):
-    results = list(Prox.objects.all().order_by('-reg_date').values())[:cnt][::-1]
-    return JsonResponse(results, safe=False)
-
-def setTemp(request):
-    try:
-        Temp.objects.create(value = request.POST['value'])
-        return JsonResponse({"message": "OK"}, status=200)
-    except KeyError:
-        return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-def setHumi(request):
-    try:
-        Humi.objects.create(value = request.POST['value'])
-        return JsonResponse({"message": "OK"}, status=200)
-    except KeyError:
-        return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-def setVib(request):
-    try:
-        Vib.objects.create(value = request.POST['value'])
-        return JsonResponse({"message": "OK"}, status=200)
-    except KeyError:
-        return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-
-def setProx(request):
-    try:
-        Prox.objects.create(value = request.POST['value'])
-        return JsonResponse({"message": "OK"}, status=200)
-    except KeyError:
-        return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
 # Create your views here.
-
-# 조도센서
-def getLightD(request, cnt):
-    results = list(LightD.objects.all().order_by('-reg_date').values())[:cnt][::-1]
+def getSensor(request, cnt):
+    results = list(Sens.objects.all().order_by('-reg_date').values())[:cnt][::-1]
     return JsonResponse(results, safe=False)
 
-def setLightD(request):
+def setSensor(request):
     try:
-        LightD.objects.create(value = request.POST['value'])
-        return JsonResponse({"message": "OK"}, status=200)
-    except KeyError:
-        return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-
-# 장애물 센서
-def getirDetect(request, cnt):
-    results = list(Detector.objects.all().order_by('-reg_date').values())[:cnt][::-1]
-    return JsonResponse(results, safe=False)
-
-def setirDetect(request):
-    try:
-        Detector.objects.create(value = request.POST['value'])
+        data = json.loads(request.body)
+        Sens.objects.create(value=data['value'],reg_date = timezone.now())
         return JsonResponse({"message": "OK"}, status=200)
     except KeyError:
         return JsonResponse({"message": "KEY_ERROR"}, status=400)
